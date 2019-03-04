4ae57be5691f7018da0a4185d72702835ac9cc7e
KonradWrbl <160815@stud.prz.edu.pl>
2019-03-04 20:26:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const PLAYER1 = 'fa-circle-o';
 const PLAYER2 = 'fa-times';
 let round = 1;
 const board = [
-    ['','',''],
-    ['','',''],
-    ['','','']
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
 ];
-
 const combinations = [
     [0,1,2], [3,4,5], [6,7,8],
-    [0,3,4], [1,4,7], [2,5,8],
+    [0,3,6], [1,4,7], [2,5,8],
     [0,4,8], [2,4,6]
 ];
 
@@ -17,7 +16,7 @@ const boxes = [...document.querySelectorAll('.box')];
 boxes.forEach(box => box.addEventListener('click', pick));
 
 function pick(event) {
-    const {row, column} = event.target.dataset;
+    const { row, column} = event.target.dataset;
     const turn = round % 2 === 0 ? PLAYER2 : PLAYER1;
     if (board[row][column] !== '') return;
     event.target.classList.add(turn);
@@ -32,7 +31,7 @@ function check() {
     let winner = null;
     let moves = {
         'fa-times': [],
-        'fa-circle-0': []
+        'fa-circle-o': []
     };
     result.forEach((field, index) => moves[field] ? moves[field].push(index) : null);
     combinations.forEach(combination => {
